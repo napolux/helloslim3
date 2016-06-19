3842c9058e23d10bd70cfa0e2f8b8ed5a8de4b45
Francesco Napoletano <napolux@gmail.com>
2016-06-19 18:08:33 +0200

    A real test

--- tests/HomepageTest.php
+++ tests/HomepageTest.php
@@ -4,18 +4,28 @@ namespace HELLO\Tests;
 require __DIR__ . '/../vendor/autoload.php';
 
 use GuzzleHttp;
+use Symfony\Component\DomCrawler\Crawler;
+
 
 class HomepageTest extends \PHPUnit_Framework_TestCase
 {
-    private $client;
+    protected $client;
 
     public function setUp()
     {
-        $this->client = new GuzzleHttp\Client();
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://localhost:8080'
+        ]);
     }
 
-    public function testStuff()
-    {
-        $this->assertTrue(true);
+    public function testHomepageIsLoadedAndGives200() {
+        $response = $this->client->request('GET', '/');
+        $this->assertEquals(200, $response->getStatusCode());
+
+        // Crawling for correct Hello World ;)
+        $crawler = new Crawler($response->getBody()->getContents());
+
+        $filter = $crawler->filter('h1');
+        $this->assertEquals('Hello, Slim 3!', $filter->text(),'Error');
     }
 }
