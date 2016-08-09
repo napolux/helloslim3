9efae3ccfdc72124e34adf5bb877272d9ee56159
Francesco Napoletano <fnapoletano@ebay.com>
2016-08-09 17:01:49 +0200

    Adding API Controller route

--- src/routes.php
+++ src/routes.php
@@ -4,3 +4,7 @@ $app->get('/', 'HELLO\Controller\HomepageController:hp')->setName('hello.website
 
 // Admin
 $app->get('/admin', 'HELLO\Controller\AdminHpController:homepage')->setName('hello.admin.hp');
+
+// APIs
+$app->get('/api', 'HELLO\Controller\ApiController:apiExample')->setName('hello.api.apiExample');
+
