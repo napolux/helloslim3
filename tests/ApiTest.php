a4f7a93cbc9019eef2f77df15fe3f13b7bbe5267
Francesco Napoletano <fnapoletano@ebay.com>
2016-08-09 17:02:03 +0200

    Adding API Controller test

---
+++ tests/ApiTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace HELLO\Tests;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp;
+use Symfony\Component\DomCrawler\Crawler;
+
+
+class ApiTest extends \PHPUnit_Framework_TestCase
+{
+    protected $client;
+
+    public function setUp()
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://localhost:8080'
+        ]);
+    }
+
+    public function testApiIsLoadedAndGives200() {
+        $data = $this->makeRequestCheckStatusAndReturnData("/api");
+        $this->assertEquals($data['thisis'], "an api response", 'API is broken!');
+
+    }
+
+    /**
+     * Making request, checking status and returning data as array
+     * @param $path
+     * @return array
+     */
+    private function makeRequestCheckStatusAndReturnData($path)
+    {
+        $response = $this->client->get($path);
+        $this->assertContains('application/json', $response->getHeader('content-type')[0], 'Not a JSON response');
+        $this->assertEquals(200, $response->getStatusCode());
+        return json_decode($response->getBody(), true);
+    }
+}
